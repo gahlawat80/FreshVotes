4bda6c4e19dae98b66496c06caf1e66d905f22ea
Pardeep Gahlawat <gahlawat80@hotmail.com>
2021-05-09 08:37:35 -0400

    create basic register page and add routing

--- src/main/java/com/freshvotes/springboot/app/controllers/HomeController.java
+++ src/main/java/com/freshvotes/springboot/app/controllers/HomeController.java
@@ -2,8 +2,12 @@ package com.freshvotes.springboot.app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+import com.freshvotes.springboot.app.entity.User;
 
 @Controller
 public class HomeController {
@@ -21,6 +25,18 @@ public class HomeController {
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String login(){
 		return "login";
+	}
+	
+	@GetMapping("/register")
+	public String register(ModelMap model){
+		model.put("user", new User());
+		return "register";
+	}
+	
+	@PostMapping("/register")
+	public String createAccount(User user){
+		System.out.println(user);
+		return "redirect:/register";
 	}
 	
 	@RequestMapping(value="/logout", method=RequestMethod.GET)
--- src/main/java/com/freshvotes/springboot/app/security/WebSecurityConfig.java
+++ src/main/java/com/freshvotes/springboot/app/security/WebSecurityConfig.java
@@ -39,6 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 		.authorizeRequests()
 			.antMatchers("/").permitAll()
+			.antMatchers("/register").permitAll()
 			.antMatchers("/admin/**").hasRole("ADMIN")
 			.anyRequest().hasAnyRole("USER","ADMIN").and()
 		.formLogin()
